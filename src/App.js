e535f31ce322ab030a9c06dd1be26b479e912e55
Eli Shields <elishields@gmail.com>
2017-05-08 11:33:15 -0700

    Implemented bootstrap in header and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import GroceryList from '../res/NavGroceryListIcon.svg';
-import MyFridge from '../res/NavFridgeIcon.png';
+import GroceryListIcon from '../res/NavGroceryListIcon.svg';
+import MyFridge from '../res/FridgeIcon.png';
 import MealPlan from '../res/NavMealPlanIcon.png';
 import Logo from '../res/Logo.png'
 import './App.css';
@@ -10,25 +10,25 @@ import '../src/bootstrap-3.3.7-dist/css/bootstrap-theme.css'
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="app">
+                <div className="container-fluid">
 
-                <div className="Header">
-                    <img src={Logo} className="Logo" alt="Meal.io"/>
+                <div className="navbar navbar-default navbar-fixed-top" id="header">
+                    <img src={Logo} className="logo" alt="Meal.io"/>
                 </div>
-                <div className="Body">
 
-                </div>
-                <div className="Nav">
-                    <div className="Icon-Group">
+                <div className="navbar navbar-default navbar-fixed-bottom">
+                    <div className="icon-group">
                         {/* GroceryList */}
-                        <a href=""><img src={GroceryList} className="Nav-Icon" alt="Grocery List"/></a>
+                        <a href=""><img src={GroceryListIcon} className="nav-icon" alt="Grocery List"/></a>
                         {/* MyFridge */}
-                        <a href=""><img src={MyFridge} className="Nav-Icon" alt="My Fridge"/></a>
+                        <a href=""><img src={MyFridge} className="nav-icon" alt="My Fridge"/></a>
                         {/* MealPlan */}
-                        <a href=""><img src={MealPlan} className="Nav-Icon" alt="Meal Plan"/></a>
+                        <a href=""><img src={MealPlan} className="nav-icon" alt="Meal Plan"/></a>
                     </div>
                 </div>
 
+                </div>
             </div>
         );
     }
