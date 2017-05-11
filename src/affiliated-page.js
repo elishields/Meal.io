5976d00cded169372fe96003039d1b2304c056bb
Eli Shields <elishields@gmail.com>
2017-05-11 15:44:08 -0700

    Updated affiliate page

--- src/affiliated-page.js
+++ src/affiliated-page.js
@@ -23,32 +23,36 @@ export class AffiliatedPage extends Component {
                 </div>
 
                 <div className="row">
-                    <div className="col-xs-10 col-xs-offset-1" id="affiliated-blurb">
+                    <div className="col-xs-10 col-xs-offset-1" id="affiliate-blurb">
                         <p>Check out these food-waste reduction apps created by BCIT students:</p>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-4 col-xs-10 col-xs-offset-1">
                         <div className="thumbnail">
-                            <a href="" className="">
-                                <img src={FoodFactoryLogo} id="first-logo" alt="Food Factory Logo"></img>
+                            <a href="">
+                                <img src={FoodFactoryLogo} className="affiliate-image" id="top-thumbnail" alt="Food Factory Logo"></img>
                             </a>
                             <div className="caption">
                                 <p>
-                                    <a href="#">Food Factory</a> is a time-based drag and drop game.
+                                    <span className="affiliate-title">Food Factory</span> is a
+                                    time-based drag and drop game.
                                     Players need to pick and match the ingredients on the belt
                                     conveyor with recipes without wasting them.
                                 </p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-4">
+                    <div className="col-md-4 col-xs-10 col-xs-offset-1">
                         <div className="thumbnail">
-                            <img src={EcoListLogo} alt="EcoList Logo"></img>
+                            <a href="http://www.eco-list.ca">
+                                <img src={EcoListLogo} className="affiliate-image" alt="EcoList Logo"></img>
+                            </a>
                             <div className="caption">
                                 <p>
-                                    <a href="http://www.eco-list.ca">EcoList</a> is a meal planner and shopping list
+                                    <span className="affiliate-title">EcoList</span> is
+                                    a meal planner and shopping list
                                     combination. Users can choose from a preset meal plan or create their own, can input
                                     items to list from meal planner.
                                 </p>
@@ -56,12 +60,14 @@ export class AffiliatedPage extends Component {
                         </div>
                     </div>
 
-                    <div className="col-md-4" id="bottom-thumbnail">
+                    <div className="col-md-4 col-xs-10 col-xs-offset-1" id="bottom-thumbnail">
                         <div className="thumbnail">
-                            <img src={Pocketpantry}  alt="PocketPantry Logo"></img>
+                            <a href="">
+                                <img src={Pocketpantry} className="affiliate-image" alt="PocketPantry Logo"></img>
+                            </a>
                             <div className="caption">
                                 <p>
-                                    <a href="#">Pocket Pantry</a> is designed to help create easy plan meal planning and
+                                    <span className="affiliate-title">Pocket Pantry</span> is designed to help create easy plan meal planning and
                                     calculation of necessary ingredients for efficient grocery lists to reduce wasteful
                                     expenditure.
                                 </p>
