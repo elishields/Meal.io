8320b6c4d159d943165d3abc18711bde63915218
Eli Shields <elishields@gmail.com>
2017-05-15 18:58:29 -0700

    Rearranged icon table into icon row.

--- src/Fridge.js
+++ src/Fridge.js
@@ -37,25 +37,19 @@ export class Fridge extends Component {
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-xs-10 col-xs-offset-1" id="fridge-col">
-                            <table className="table" id="fridge-table">
-                                <tr className="fridge-table-row">
-                                    <td className="fridge-category">
-                                        <img className="fridge-category-icon" src={FruitVeg} alt="FruitVeg"/>
-                                    </td>
-                                    <td className="fridge-category">
-                                        <img className="fridge-category-icon" src={Dairy} alt="Dairy"/>
-                                    </td>
-                                </tr>
-                                <tr className="fridge-table-row">
-                                    <td className="fridge-category">
-                                        <img className="fridge-category-icon" src={Meat} alt="Meat"/>
-                                    </td>
-                                    <td className="fridge-category">
-                                        <img className="fridge-category-icon" src={OtherIcon} alt="OtherIcon"/>
-                                    </td>
-                                </tr>
-                            </table>
+                        <div className="col-xs-12" id="fridge-icon-row">
+                            <div className="col-xs-3 fridge-category-col">
+                                <img className="fridge-category-icon" src={FruitVeg} alt="FruitVeg"/>
+                            </div>
+                            <div className="col-xs-3 fridge-category-col">
+                                <img className="fridge-category-icon" src={Dairy} alt="Dairy"/>
+                            </div>
+                            <div className="col-xs-3 fridge-category-col">
+                                <img className="fridge-category-icon" src={Meat} alt="Meat"/>
+                            </div>
+                            <div className="col-xs-3 fridge-category-col">
+                                <img className="fridge-category-icon" src={OtherIcon} alt="OtherIcon"/>
+                            </div>
                         </div>
                     </div>
                 </div>
