54038389a9d8a1bc0deb5a937bab078cdc327f54
prabhsaran <prabhvirsaran@gmail.com>
2017-05-19 00:29:18 -0700

    used nested columns to fix the styling on about-us

--- src/about-us.js
+++ src/about-us.js
@@ -28,13 +28,25 @@ export class AboutusPage extends Component {
                     </div>
 
                     <div className="row">
-                        <div className="col-xs-12 col-md-6 col-md-offset-3">
-                            <p id="first-paragraph">
-                                Meal.io is your new meal manager!<br/><br/>
-                                Use it to make grocery lists, track perishable foods in your fridge, and create meal plans!<br/><br/>
-                                We hope it helps you waste less food!
+
+                        <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                            <p>
+                                Meal.io is your new meal manager!
                             </p>
                         </div>
+
+                        <div className="row">
+                            <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                                Use it to make grocery lists, track perishable foods in your fridge, and create meal plans!
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                                We hope it helps you waste less food!
+                            </div>
+                        </div>
+
                     </div>
 
                     <br></br>
@@ -43,10 +55,16 @@ export class AboutusPage extends Component {
                         <div className="col-xs-3 col-xs-offset-1 col-md-2 col-md-offset-3">
                             <img className="three-icons" src={GroceryListIcon} alt="Grocery List Logo"></img>
                         </div>
-                        <div className="col-xs-8 col-lg-4">
-                            Plan ahead by writing up a grocery list before you go shopping.<br/><br/>
-                            Once you've bought your groceries, simply tap to send them to your fridge!
+                        <div className="col-xs-7 col-lg-4">
+                            Plan ahead by writing up a grocery list before shopping.
+                            <div className="row">
+
+                                <div className="col-xs-12">
+                                    Once you've bought your groceries, simply tap to send them to your fridge!
+                                </div>
+                            </div>
                         </div>
+
                     </div>
 
                     <br></br>
@@ -57,10 +75,16 @@ export class AboutusPage extends Component {
                             <img className="three-icons" src={MyFridgeIcon} alt="Fridge Logo"></img>
                         </div>
 
-                        <div className="col-xs-8 col-lg-4">
-                            Your fridge has 4 categories: Fruits & Veggies, Dairy, Meats, and Other items.<br/><br/>
-                            Find your items by simply scrolling through your inventory, or tap the icons to jump to them!
+                        <div className="col-xs-7 col-lg-4">
+                            Your fridge has 4 categories: Fruits & Veggies, Dairy, Meats, and Other items.
+                            <div className="row">
+
+                                <div className="col-xs-12">
+                                    Find your items by simply scrolling through your inventory, or tap the icons to jump to them!
+                                </div>
+                            </div>
                         </div>
+
 
                     </div>
 
@@ -71,37 +95,62 @@ export class AboutusPage extends Component {
                         <div className="col-xs-3 col-xs-offset-1 col-md-2 col-md-offset-3">
                             <img className="three-icons" src={MealPlanningIcon} alt="Meal Planning Logo"></img>
                         </div>
-                        <div className="col-xs-8 col-lg-4">
-                            Dream up delicious meals using the Meal Planner!<br/><br/>
-                            It's easy to add foods from your fridge!
-                        </div>
-                    </div>
+                        <div className="col-xs-7 col-lg-4">
+                            Dream up delicious meals using the Meal Planner!
+                            <div className="row">
 
-                    <br></br>
-
-
-                    <div className="row" id="team">
-
-                        <div className="col-sm-12 col-md-6 col-md-offset-3">
-
-                            <h4 className="page-header" id="second-heading">
-                                <span className="page-title-text">Meet the Team</span>
-                            </h4>
-
-                            <div className="thumbnail" id="clear-thumbnail-border">
-                                <img src={TeamPicture} id="aboutus-team-image" alt="Team Picture"></img>
-                                <div className="caption">
-                                    <p>
-                                        Meal.io was created by a small-team of dedicated Computer Systems Technology students
-                                        at BCIT!<br/><br/>
-                                        Built for Metro Vancouver's food-waste reduction plan,
-                                        Meal.io offers users a simple way to manage the food in their lives!<br/><br/>
-                                        We built this fun app using ReactJS, Bootstrap, and Firebase. Please hire us!
-                                    </p>
+                                <div className="col-xs-12">
+                                    It's easy to add foods from your fridge!
                                 </div>
                             </div>
                         </div>
+
                     </div>
+
+
+
+                    <br></br>
+
+                    <div className="row">
+                        <div className="col-xs-12 col-md-6 col-md-offset-3">
+                            <h4 className="page-header" id="second-heading">
+                                <span className="page-title-text">Meet the Team</span>
+                            </h4>
+                        </div>
+                    </div>
+
+                    <div className="row">
+
+                        <div className="col-xs-9 col-xs-offset-3 col-md-6 col-md-offset-4">
+                            <p>
+                                <img src={TeamPicture} id="aboutus-team-image" alt="Team Picture"></img>
+                            </p>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                                Meal.io was created by a small-team of dedicated Computer Systems Technology students
+                                at BCIT!
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                                Built for Metro Vancouver's food-waste reduction plan,
+                                Meal.io offers users a simple way to manage the food in their lives!
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3">
+                                We built this fun app using ReactJS, Bootstrap, and Firebase. Please hire us!
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <br></br>
 
                     <div className="row" >
                         <div className="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-3" >
